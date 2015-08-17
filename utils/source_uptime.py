b0927b1dcd7779192811c6bc7c3596d51529acac
Erik Brozek <erik.brozek@ni.cz>
2015-08-17 10:33:27 +0200

    update format + simple:)

--- utils/source_uptime.py
+++ utils/source_uptime.py
@@ -5,11 +5,11 @@
 	This example uses siux lib to connect to monitored server.
 """
 
-import  sys, pprint,time, datetime
+import  sys, pprint,time, datetime, urlparse
 sys.path.append( '../src' )
 import siuxlib
 
-if len(sys.argv) > 1:
+if len(sys.argv) <= 1:
 	print "Usage source_uptime.py www.yoursource.com"
 	sys.exit(1)
 
@@ -20,46 +20,61 @@ auth = '<YOUR_API_KEY>'
 S = siuxlib.SiUXclient( auth=auth )
 
 # make request to server
-retList = S.sourceList(auth)
+retList = S.sourceList()
 
-#print return value in case of debugging
-#pprint.pprint(retList)
+# print return value in case of debugging
+#pprint.pprint( retList )
 
 # current time settings
-currentTimestamp = time.time()
+tsNow = int( time.time() )
 
-#check response code from server
-if retList['statusCode'] == 'OK':
+# check response code from server
+if retList['statusCode'] != 'OK':
+	pprint.pprint( retList )
+	sys.exit(1)
 
-	for line in retList['data']:
+# data from api -> sourceList()
+for line in retList['data']:
 
-		url = line['url'].replace("http://","").replace("https://","").replace("/","")
+	# from url address to hostname
+	hostname = urlparse.urlparse( line['url'] ).netloc
 
-		if sys.argv[1] == url:
+	if sys.argv[1] != hostname:
+		continue
 
-			errTime = line['lastErrChecktime']
-			uptimeText = " "
-			# if last return value is not 200 OK
-			if errTime != 0:
+	errTime 	= line['lastErrChecktime']
+	sourceId	= line[ 'sourceId' ]
+	url		= line[ 'url' ]
+	state		= line[ 'status' ]
 
-					retList2 = retList = S.sourceInfo(auth,line['sourceId'])
+	# only active source
+	if state != 'active':
+		continue
 
-					#pprint.pprint(retList2)
+	# if last return value is not 200 OK
+	if errTime == 0:
+		print '%s | %s | %s' % (0, 0, url)
+		continue
 
-					errTime2 = retList2['data']['lastErrChecktime']
+	retInfo = S.sourceInfo( sourceId )
 
-					uptimeSeconds=round(currentTimestamp-errTime2)
+	# error status for sourceInfo()
+	if retInfo[ 'statusCode' ] != 'OK':
+		pprint.pprint( retInfo )
+		continue
 
-					timeText="%s s"%uptimeSeconds
+	errTime2 = retInfo['data']['lastErrChecktime']
+	uptimeSeconds = round( tsNow - errTime2 )
+	timeText = '%s s' % uptimeSeconds
 
-					if uptimeSeconds > 60 and uptimeSeconds < 3600:
-						timeText="%s m"%(round(uptimeSeconds/60))
-					elif uptimeSeconds >= 3600 and uptimeSeconds < 86400:
-						timeText="%s h"%(round(uptimeSeconds/3600))
-					elif uptimeSeconds >= 84000 :
-						timeText="%s d"%(round(uptimeSeconds/84000))
+	if uptimeSeconds > 60 and uptimeSeconds < 3600:
+		timeText="%s m" %  int(uptimeSeconds/60)
+	elif uptimeSeconds >= 3600 and uptimeSeconds < 86400:
+		timeText="%s h" % int(uptimeSeconds/3600)
+	elif uptimeSeconds >= 84000 :
+		timeText="%s d" % int(uptimeSeconds/84000)
 
-					uptimeText2 = "%s -  %s"%(timeText,datetime.datetime.fromtimestamp(errTime2).strftime('%Y-%m-%d %H:%M:%S'))
-					print (line['url'],uptimeText2)
-			else:
-				print (line['url'],uptimeText)
+	print '%s | %s | %s' % (datetime.datetime.fromtimestamp(errTime2).strftime('%Y-%m-%d %H:%M:%S'), timeText, url )
+
+print
+print '.done'
