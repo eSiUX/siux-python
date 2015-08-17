ba3935f52b269449013809f4add7dc20ad3461dc
martinkala <martinkala@outlook.com>
2015-08-17 09:05:31 +0200

    small code style issues

--- utils/source_uptime.py
+++ utils/source_uptime.py
@@ -10,59 +10,56 @@ sys.path.append( '../src' )
 import siuxlib
 
 if len(sys.argv) > 1:
-	# config
-	auth = '<YOUR_API_KEY>'
-
-	# init
-	S = siuxlib.SiUXclient( auth=auth )
-
-	# make request to server
-	retList = S.sourceList(auth)
-
-	#print return value in case of debugging
-	#pprint.pprint(retList)
-
-	errors = 0
-
-	# current time settings
-	currentTimestamp = time.time()
-
-	#check response code from server
-	if retList['statusCode'] == 'OK':
-
-		for line in retList['data']:
-
-			url = line['url'].replace("http://","").replace("https://","").replace("/","")
-
-			if sys.argv[1] == url:
-
-				errTime = line['lastErrChecktime']
-				uptimeText = " "
-				# if last return value is not 200 OK
-				if errTime != 0:
-
-						retList2 = retList = S.sourceInfo(auth,line['sourceId'])
-
-						#pprint.pprint(retList2)
-
-						errTime2 = retList2['data']['lastErrChecktime']
-
-						uptimeSeconds=round(currentTimestamp-errTime2)
-
-						timeText="%s s"%uptimeSeconds
-
-						if uptimeSeconds > 60 and uptimeSeconds < 3600:
-							timeText="%s m"%(round(uptimeSeconds/60))
-						elif uptimeSeconds >= 3600 and uptimeSeconds < 86400:
-							timeText="%s h"%(round(uptimeSeconds/3600))
-						elif uptimeSeconds >= 84000 :
-							timeText="%s d"%(round(uptimeSeconds/84000))
-
-						uptimeText2 = "%s -  %s"%(timeText,datetime.datetime.fromtimestamp(errTime2).strftime('%Y-%m-%d %H:%M:%S'))
-						print (line['url'],uptimeText2)
-				else:
-					print (line['url'],uptimeText)
-
-else:
 	print "Usage source_uptime.py www.yoursource.com"
+	sys.exit(1)
 
+# config
+auth = '<YOUR_API_KEY>'
+
+# init
+S = siuxlib.SiUXclient( auth=auth )
+
+# make request to server
+retList = S.sourceList(auth)
+
+#print return value in case of debugging
+#pprint.pprint(retList)
+
+# current time settings
+currentTimestamp = time.time()
+
+#check response code from server
+if retList['statusCode'] == 'OK':
+
+	for line in retList['data']:
+
+		url = line['url'].replace("http://","").replace("https://","").replace("/","")
+
+		if sys.argv[1] == url:
+
+			errTime = line['lastErrChecktime']
+			uptimeText = " "
+			# if last return value is not 200 OK
+			if errTime != 0:
+
+					retList2 = retList = S.sourceInfo(auth,line['sourceId'])
+
+					#pprint.pprint(retList2)
+
+					errTime2 = retList2['data']['lastErrChecktime']
+
+					uptimeSeconds=round(currentTimestamp-errTime2)
+
+					timeText="%s s"%uptimeSeconds
+
+					if uptimeSeconds > 60 and uptimeSeconds < 3600:
+						timeText="%s m"%(round(uptimeSeconds/60))
+					elif uptimeSeconds >= 3600 and uptimeSeconds < 86400:
+						timeText="%s h"%(round(uptimeSeconds/3600))
+					elif uptimeSeconds >= 84000 :
+						timeText="%s d"%(round(uptimeSeconds/84000))
+
+					uptimeText2 = "%s -  %s"%(timeText,datetime.datetime.fromtimestamp(errTime2).strftime('%Y-%m-%d %H:%M:%S'))
+					print (line['url'],uptimeText2)
+			else:
+				print (line['url'],uptimeText)
